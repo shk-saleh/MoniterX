6a31da2f5da09f141f5b1433daaf1141b983832f
gptone-me <sanabano.gptone@gmail.com>
2025-11-24 18:30:18 +0500

    added project source files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Dashboard from './Pages/Dashboard.jsx'
+
+function App() {
+
+  return (
+      <div className='w-full bg-white min-h-screen'>
+        <Dashboard/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/Components/Main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Overview from './Overview';
+import { CPU } from './CPU';
+
+const Main = ({activeTab}) => {
+
+
+  const renderTab = () =>{
+    switch (activeTab) {
+      case 'overview':
+         return <Overview/>
+      case 'cpu':
+         return <CPU/>  
+      default:
+        break;
+    }
+  }
+
+
+  return (
+    <div className='w-full h-auto'>
+
+      {/* Dashboard Header  */}
+
+      <div className='sticky top-0 flex justify-between items-center bg-white border-b border-gray-200 p-4'>
+        <span>Moniter what's going on! </span>
+        <div className='flex relative gap-2'>
+          <span class="relative flex items-center">
+            <span class="absolute inline-flex size-2 animate-ping rounded-full bg-purple-400 opacity-75"></span>
+            <span class="relative inline-flex size-2 rounded-full bg-purple-500"></span>
+          </span>
+          <span className='text-sm'>Server Connected</span>
+        </div>
+      </div>
+      <div className='bg-(--primary-color)/4 h-auto p-4'>
+        {renderTab()}
+      </div>
+    </div>
+  )
+}
+
+export default Main
